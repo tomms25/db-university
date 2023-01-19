f4eed71c7dec16d42765d6528a381e776ef25eb0
tomms25 <tommasobaccano@gmail.com>
2023-01-19 16:12:15 +0100

    Query completate

--- query_uni.sql
+++ query_uni.sql
@@ -13,4 +13,28 @@ SELECT*
 FROM `students`
 WHERE `date_of_birth` > 1992-12-31;
 
--- Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
+-- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
+SELECT *
+FROM `courses`
+WHERE `year` = 1
+AND `period` = 'I semestre';
+
+-- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
+SELECT * 
+FROM `exams`
+WHERE `hour` >= '14:00:00'
+AND `date` = '2020-06-20';
+
+-- 6. Selezionare tutti i corsi di laurea magistrale (38)
+SELECT * 
+FROM `degrees`
+WHERE `level` = 'magistrale';
+
+-- 7. Da quanti dipartimenti è composta l'università? (12)
+SELECT COUNT(*) 
+FROM `departments`;
+
+-- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
+SELECT COUNT(*)
+FROM `teachers`
+WHERE `phone` IS NULL;
